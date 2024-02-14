1334ded6d04ef9b3a40771f5432bf29972ff829d
Yakiv Brychuk <yakiv.brychuk0@gmail.com>
2024-02-14 16:46:01 +0000

    Wire up APis and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
-# Your code goes here.
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
+]
 
+creds = Credentials.from_service_account_file('creds.json', scopes=SCOPES)
+client = gspread.authorize(creds)
+sheet = client.open('satoshis_oven')
+pizzas = sheet.worksheet('pizzas')
 
-
+data = pizzas.get_all_values()
+print(data)
